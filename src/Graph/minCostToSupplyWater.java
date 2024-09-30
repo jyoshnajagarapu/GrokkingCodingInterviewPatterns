34fed2c36d91366da8c3f82cf097dc7ae82b54ca
Jyothsna Jagarapu <jagarapujyothsna@gmail.com>
2024-09-30 14:08:55 +0530

    minimum cost to supply water

---
+++ src/Graph/minCostToSupplyWater.java
@@ -0,0 +1,67 @@
+package Graph;
+
+// Java program for the above approach
+
+import java.util.*;
+
+ class Main {
+     static int parent[];
+     static int rank[];
+     static boolean connect(int v1, int v2){
+         int p1 = find(v1);
+         int p2 = find(v2);
+         if(p1 == p2) return false;
+         if(rank[p1] >= rank[p2]){
+             parent[p2] = p1;
+             rank[p1] += rank[p2];
+         } else{
+             parent[p1] = p2;
+             rank[p2] +=rank[p1];
+         }
+         return true;
+     }
+
+     static int find(int p1){
+         if(parent[p1] != p1){
+             parent[p1] = find(parent[p1]);
+         }
+         return parent[p1];
+     }
+
+
+    // Function to solve the problem
+    static int minCostToProvideWater(int n, int[] wells, int[][] pipes) {
+         parent = new int[n+1];
+         rank  =new int[n+1];
+         int res = 0 ;
+         int[][] newpipe = Arrays.copyOf(pipes,pipes.length + n);
+         int i = pipes.length;
+         for(int j = 0 ; j< wells.length; j++){
+             newpipe[i++] = new int[]{0,j+1,wells[j]};
+         }
+        for(int k = 0; k <= n ; k++){
+            parent[k] = k ;
+            rank[k] = k;
+        }
+        Arrays.sort(newpipe, Comparator.comparingInt(a -> a[2]));
+        for(int[] a : newpipe) {
+            if (connect(a[0], a[1])) {
+                res += a[2];
+            }
+        }
+
+         return res ;
+    }
+
+    // Driver Function
+    public static void main(String[] args) {
+        int N = 3;
+        int[] wells = new int[]{1, 2, 2};
+        int[][] pipes = new int[][]{{1, 2, 1},{2, 3, 1}};
+
+        // Function call
+        System.out.println(minCostToProvideWater(N, wells, pipes));
+    }
+}
+
+// This code is contributed by Abhinav Mahajan (abhinav_m22).
