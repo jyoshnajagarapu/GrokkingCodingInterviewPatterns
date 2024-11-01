58783d3884cb986aa34d9271400d5f8154d891dc
Jyothsna Jagarapu <jagarapujyothsna@gmail.com>
2024-11-01 15:15:34 +0530

    minimumCost

--- src/Graph/MinimumCost.java
+++ src/Graph/MinimumCost.java
@@ -61,3 +61,64 @@ public class MinimumCost {
         System.out.println("The minimum cost is: " + minCost);
     }
 }
+/*
+There are n cities labeled from 1 to n. You are given the integer n and an array connections where connections[i] = [xi, yi, costi] indicates that the cost of connecting city xi and city yi (bidirectional connection) is costi.
+
+Return the minimum cost to connect all the n cities such that there is at least one path between each pair of cities. If it is impossible to connect all the n cities, return -1,
+
+The cost is the sum of the connections' costs used
+
+https://leetcode.com/problems/connecting-cities-with-minimum-cost/
+
+class Solution {
+
+    public int minimumCost(int n, int[][] connections) {
+        Arrays.sort(connections, (a,b) -> a[2] - b[2]);
+        UnionFind uf = new UnionFind(n);
+        int res = 0 ;
+        int components = n ;
+        for(int[] a : connections){
+            if(uf.connect(a[0], a[1])){
+                res+=a[2];
+                components--;
+            }
+        }
+        //System.out.println("res" +  res);
+        if(components != 1) return -1;
+        return res;
+    }
+}
+
+class UnionFind{
+    int parent[];
+    int rank[];
+    UnionFind(int n){
+        parent = new int[n+1];
+        rank = new int[n+1];
+        for(int i = 1; i <= n ; i++){
+            parent[i] = i ;
+            rank[i] = 1;
+        }
+    }
+    boolean connect(int v1, int v2){
+        int p1 = find(v1);
+        int p2 = find(v2);
+        if(p1 == p2) return false;
+        if(rank[p1] >= rank[p2]){
+            parent[p2] = p1;
+            rank[p1] += rank[p2];
+        } else{
+            parent[p1] = p2;
+            rank[p2] +=rank[p1];
+        }
+        return true;
+    }
+
+    int find(int p1){
+        if(parent[p1] != p1){
+            parent[p1] = find(parent[p1]);
+        }
+        return parent[p1];
+    }
+}
+*/
