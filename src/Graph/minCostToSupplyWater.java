d83c81e6bde7147cc6721509bb2733791c4e7177
Jyothsna Jagarapu <jagarapujyothsna@gmail.com>
2024-11-01 15:23:19 +0530

    minCostToProvideWater

--- src/Graph/minCostToSupplyWater.java
+++ src/Graph/minCostToSupplyWater.java
@@ -64,4 +64,57 @@ import java.util.*;
     }
 }
 
-// This code is contributed by Abhinav Mahajan (abhinav_m22).
+//https://leetcode.com/problems/optimize-water-distribution-in-a-village/
+
+//class Solution {
+//    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+//        UnionFind uf = new UnionFind(n);
+//        int res = 0 ;
+//        int[][] newpipe = Arrays.copyOf(pipes,pipes.length + n);
+//        int i = pipes.length;
+//        for(int j = 0 ; j< wells.length; j++){
+//            newpipe[i++] = new int[]{0,j+1,wells[j]};
+//        }
+//        Arrays.sort(newpipe, Comparator.comparingInt(a -> a[2]));
+//        for(int[] a : newpipe) {
+//            if (uf.connect(a[0], a[1])) {
+//                res += a[2];
+//            }
+//        }
+//        return res ;
+//
+//    }
+//}
+//
+//class UnionFind{
+//    int parent[];
+//    int rank[];
+//    UnionFind(int n){
+//        parent = new int[n+1];
+//        rank = new int[n+1];
+//        for(int i = 0; i <= n ; i++){
+//            parent[i] = i ;
+//            rank[i] = 1;
+//        }
+//    }
+//    boolean connect(int v1, int v2){
+//        int p1 = find(v1);
+//        int p2 = find(v2);
+//        if(p1 == p2) return false;
+//        if(rank[p1] >= rank[p2]){
+//            parent[p2] = p1;
+//            rank[p1] += rank[p2];
+//        } else{
+//            parent[p1] = p2;
+//            rank[p2] +=rank[p1];
+//        }
+//        return true;
+//    }
+//
+//    int find(int p1){
+//        if(parent[p1] != p1){
+//            parent[p1] = find(parent[p1]);
+//        }
+//        return parent[p1];
+//    }
+//}
